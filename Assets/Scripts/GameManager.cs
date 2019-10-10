caedc6901c0bbd6e9cf60153633e6ef90342e4ca
orlando <orlax22@gmail.com>
2019-10-10 12:20:59 -0500

    added initial UI implementation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float Energy = 100;
+    public float EnergyUseRate = 1;
+    public float Overload = 0;
+    public float OverloadRate = 10;
+
+    private bool isOverloading = false;
+    private bool GameOver = false;
+
+    UIManager UI; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI = FindObjectOfType<UIManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameOver) return; 
+
+        Energy = Energy - EnergyUseRate * Time.deltaTime; //cada segundo que pasa perdemos energia. 
+        if (Energy <= 0 && !GameOver)
+        {
+            GameOverScreen(); 
+        }
+
+        //Actualizar la UI si hay alguna. 
+        if (UI != null)
+        {
+            UI.Energy = Energy;
+            UI.Overload = Overload; 
+        }
+    }
+
+    internal void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+
+    internal void GoToHomeScreen()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GameOverScreen()
+    {
+        GameOver = true; 
+        UI?.GameOverScreen(); 
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode]
+public class UIManager : MonoBehaviour
+{
+
+
+    public float Energy = 100;
+    public float Overload = 0;
+
+    public Image energyBar;
+    public Image overloadBar;
+
+    public GameObject OverloadPauseCanvas;
+
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        energyBar.rectTransform.localScale = new Vector3(Energy / 100, 1, 1);
+        overloadBar.rectTransform.localScale = new Vector3(Overload / 100, 1, 1);
+    }
+
+    internal void GameOverScreen()
+    {
+        OverloadPauseCanvas.SetActive(true);
+    }
+
+    public void RestarButtonClicked()
+    {
+        gameManager?.RestartLevel();           
+    }
+
+    public void HomeButtonClicked()
+    {
+        gameManager?.GoToHomeScreen();
+    }
+}
